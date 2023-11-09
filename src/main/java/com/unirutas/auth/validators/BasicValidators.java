8e6f8080f711e2d0a76ef731629fcd3ddf6615fb
JhMateo <jmgranada@unillanos.edu.co>
2023-11-08 23:36:23 -0500

    Added documentation

--- src/main/java/com/unirutas/auth/validators/BasicValidators.java
+++ src/main/java/com/unirutas/auth/validators/BasicValidators.java
@@ -4,17 +4,43 @@ import com.unirutas.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+/**
+ * BasicValidators provide validation methods for basic user authentication.
+ */
 public class BasicValidators {
     private static final Logger logger = LoggerFactory.getLogger(BasicValidators.class);
 
+    /**
+     * Checks if the user authentication is basic (no phone or security phrase).
+     *
+     * @param user The user to check.
+     * @return True if the authentication is basic, false otherwise.
+     */
     public static boolean validateIsBasic(User user){
         return user.getSecurityPhrase() == null && user.getPhone() == null;
     }
 
+    /**
+     * Validates basic credentials (username and password).
+     *
+     * @param user     The user to authenticate.
+     * @param username The username for authentication.
+     * @param password The password for authentication.
+     * @return True if authentication is successful, false otherwise.
+     */
     public static boolean basicValidation(User user, String username, String password){
         return user.getUsername().equals(username) && user.getPassword().equals(password);
     }
 
+    /**
+     * Validates user credentials and logs authentication results.
+     *
+     * @param user     The user to authenticate.
+     * @param username The username for authentication.
+     * @param password The password for authentication.
+     * @return True if authentication is successful, false otherwise.
+     */
     public static boolean validateCredentials(User user, String username, String password){
         logger.info("Validating credentials...");
         if (basicValidation(user, username, password)){
--- src/main/java/com/unirutas/auth/validators/PhoneValidators.java
+++ src/main/java/com/unirutas/auth/validators/PhoneValidators.java
@@ -4,17 +4,43 @@ import com.unirutas.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+/**
+ * PhoneValidators provide validation methods for phone-based user authentication.
+ */
 public class PhoneValidators {
     private static final Logger logger = LoggerFactory.getLogger(PhoneValidators.class);
 
+    /**
+     * Checks if the user has a phone and no security phrase.
+     *
+     * @param user The user to check.
+     * @return True if the user has a phone and no security phrase, false otherwise.
+     */
     public static boolean validateIsPhone(User user) {
         return user.getPhone() != null && user.getSecurityPhrase() == null;
     }
 
+    /**
+     * Validates phone-based credentials.
+     *
+     * @param user  The user to authenticate.
+     * @param phone The phone number for authentication.
+     * @return True if authentication is successful, false otherwise.
+     */
     public static boolean phoneValidation(User user, String phone) {
         return user.getPhone().equals(phone);
     }
 
+    /**
+     * Validates user credentials and phone, logging authentication results.
+     *
+     * @param user     The user to authenticate.
+     * @param username The username for authentication.
+     * @param password The password for authentication.
+     * @param phone    The phone number for authentication.
+     * @return True if authentication is successful, false otherwise.
+     */
     public static boolean validateCredentialsAndPhone(User user, String username, String password, String phone) {
         logger.info("Validating credentials and phone...");
         if (BasicValidators.basicValidation(user, username, password) && phoneValidation(user, phone)){
